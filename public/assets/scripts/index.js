aa8306f89a709c790e7119df813f644264aed1b8
Eric Bossler <eric.bossler@gmail.com>
2020-04-21 22:07:27 -0700

    started file structure

---
+++ public/assets/scripts/index.js
@@ -0,0 +1,8 @@
+// REQUIRE THE QUESTIONS
+const currentEventQuestions = require('../../../quiz/currentevents');
+const herstoryQuestions = require('../../../quiz/herstory');
+const hollywoodyQuestions = require('../../../quiz/hollywoody');
+const lostAndFoundQuestions = require('../../../quiz/lostandfound');
+const scienceQuestions = require('../../../quiz/science');
+const sportsBallQuestions = require('../../../quiz/sportsball');
+
